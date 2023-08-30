bb72df780b671f5f41852b7c0176b1b91b66af90
samotazky <samotazky82@gmail.com>
2023-08-30 17:57:05 +0200

    premena do px

--- crew.js
+++ crew.js
@@ -1,3 +1,13 @@
+//MENU
+let menuBtn = document.querySelector(".menu-btn")
+let btn1 = document.querySelector(".bar-1")
+let menu = document.querySelector(".line-with-nav")
+
+document.querySelector(".menu-btn").onclick = function (event) {
+    menuBtn.classList.toggle("change")
+    menu.classList.toggle("is-active")
+}
+
 const employeesArray = [{
     name: "Douglas Hurley",
     focus:"Commander ",
