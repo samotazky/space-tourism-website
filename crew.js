f25bcf530e43b8a5e478a28e047681ec3cf0f206
samotazky <samotazky82@gmail.com>
2023-09-02 15:59:04 +0200

    opravy js a dokoncenie

--- crew.js
+++ crew.js
@@ -12,22 +12,22 @@ const employeesArray = [{
     name: "Douglas Hurley",
     focus:"Commander ",
     text: "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
-    image: "/starter-code/assets/crew/image-douglas-hurley.png",
+    image: "url('/starter-code/assets/crew/image-douglas-hurley.png",
 }, {
     name: "MARK SHUTTLEWORTH",
     focus:"Mission Specialist",
     text: "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.",
-    image: "/starter-code/assets/crew/image-mark-shuttleworth.png"
+    image: "url('/starter-code/assets/crew/image-mark-shuttleworth.png')"
 }, {
     name: "Victor Glover",
     focus:"PILOT",
     text: "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. ",
-    image: "/starter-code/assets/crew/image-victor-glover.png"
+    image: "url('/starter-code/assets/crew/image-victor-glover.png')"
 }, {
     name: "Anousheh Ansari",
     focus:"Flight Engineer",
     text: "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.",
-    image: "/starter-code/assets/crew/image-anousheh-ansari.png"
+    image: "url('/starter-code/assets/crew/image-anousheh-ansari.png')"
 }]
 
 let douglasBtn = document.getElementById("douglas")
@@ -39,59 +39,39 @@ let anoushehBtn = document.getElementById("anousheh")
 let employeesFocus = document.querySelector(".left-side h3")
 let employeesName = document.querySelector(".left-side h1")
 let employeesText = document.querySelector(".left-side p")
-let employeesImage = document.querySelector(".right-side img")
+let employeesImage = document.querySelector(".right-side")
 
 
 douglasBtn.addEventListener("click", () => {
 employeesFocus.textContent= employeesArray[0].focus
 employeesName.textContent= employeesArray[0].name
 employeesText.textContent = employeesArray[0].text
-employeesImage.src = employeesArray[0].image
-
-// employeesImage.style.width = "568.072px"
-// employeesImage.style.height = "712px"
+employeesImage.style.backgroundImage = employeesArray[0].image
 })
 
 markBtn.addEventListener("click", () => {
     employeesFocus.textContent= employeesArray[1].focus
     employeesName.textContent= employeesArray[1].name
     employeesText.textContent = employeesArray[1].text
-    employeesImage.src = employeesArray[1].image
-
-    // employeesImage.style.width = "453.44px"
-    // employeesImage.style.height = "654px"
+    employeesImage.style.backgroundImage = employeesArray[1].image
     })
 
 victorBtn.addEventListener("click", () => {
     employeesFocus.textContent= employeesArray[2].focus
     employeesName.textContent= employeesArray[2].name
     employeesText.textContent = employeesArray[2].text
-    employeesImage.src = employeesArray[2].image
-
-    // employeesImage.style.width = "554.388px"
-    // employeesImage.style.height = "681px"
+    employeesImage.style.backgroundImage = employeesArray[2].image
     })
 
 anoushehBtn.addEventListener("click", () => {
     employeesFocus.textContent= employeesArray[3].focus
      employeesName.textContent= employeesArray[3].name
     employeesText.textContent = employeesArray[3].text
-    employeesImage.src = employeesArray[3].image
-    
-    // employeesImage.style.width = "615.567px"
-    // employeesImage.style.height = "607px"
+    employeesImage.style.backgroundImage = employeesArray[3].image
     })
 
 
 let employeesCircles = document.querySelectorAll(".crew-nav .employees");
-
-/*
-function updateEmployeeInfo(index) {
-    employeesFocus.textContent = employeesArray[index].focus
-    employeesName.textContent = employeesArray[index].name
-    employeesText.textContent = employeesArray[index].text
-    employeesImage.src = employeesArray[index].image
-}*/
 
 employeesCircles.forEach(circle => {
     circle.addEventListener("click", () => {
@@ -102,8 +82,5 @@ employeesCircles.forEach(circle => {
                 otherCircle.style.opacity = 0.174363;
             }
         });
-/*
-        let index = Array.from(employeesCircles).indexOf(circle);
-        updateEmployeeInfo(index);*/
     });
 });
--- technology.js
+++ technology.js
@@ -12,15 +12,15 @@ document.querySelector(".menu-btn").onclick = function (event) {
 const technologyArray = [{
     heading: "LAUNCH VEHICLE",
     text: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
-    image: "/starter-code/assets/technology/image-launch-vehicle-portrait.jpg"
+    image: "url('/starter-code/assets/technology/image-launch-vehicle-portrait.jpg')"
 }, {
     heading: "SPACEPORT",
     text: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
-    image: "/starter-code/assets/technology/image-spaceport-portrait.jpg"
+    image: "url('/starter-code/assets/technology/image-spaceport-portrait.jpg')"
 }, {
     heading: "SPACE CAPSULE",
     text: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
-    image: "/starter-code/assets/technology/image-space-capsule-portrait.jpg"
+    image: "url('/starter-code/assets/technology/image-space-capsule-portrait.jpg')"
 }]
 
 let btn = document.querySelectorAll(".btn")
@@ -47,22 +47,23 @@ let btn3 = document.getElementById("btn-3")
 
 let heading = document.querySelector(".right h1")
 let text = document.querySelector(".right p")
-let image = document.querySelector(".right-side img")
+let image = document.querySelector(".right-side")
+
 
 btn1.addEventListener("click", () => {
     heading.textContent = technologyArray[0].heading
     text.textContent = technologyArray[0].text
-    image.src = technologyArray[0].image
+    image.style.backgroundImage = technologyArray[0].image
 })
 
 btn2.addEventListener("click", () => {
     heading.textContent = technologyArray[1].heading
     text.textContent = technologyArray[1].text
-    image.src = technologyArray[1].image
+    image.style.backgroundImage = technologyArray[1].image
 })
 
 btn3.addEventListener("click", () => {
     heading.textContent = technologyArray[2].heading
     text.textContent = technologyArray[2].text
-    image.src = technologyArray[2].image
+    image.style.backgroundImage = technologyArray[2].image
 })
